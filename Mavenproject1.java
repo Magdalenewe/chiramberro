ac4bac2cee3e3d6c79d736c771fcd83dd63138cf
Magdalenewe <127776996+Magdalenewe@users.noreply.github.com>
2023-03-13 16:09:49 -0300

    Add files via upload

---
+++ Mavenproject1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Alumno
+ */
+import java.util.*;
+public class Mavenproject1 {
+ 
+    public static void main(String[] args) {
+       
+        Scanner sc= new Scanner(System.in);
+        
+        System.out.println("ingrese un número");
+        int var = sc.nextInt();
+        System.out.println(var);
+    }
+}
