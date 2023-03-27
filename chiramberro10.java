7b5582c1f394860beab8823158b0bbff32ab0adb
Magdalene <127776996+Magdalenewe@users.noreply.github.com>
2023-03-27 06:22:23 +0000

    Add files via upload

---
+++ chiramberro10.java
@@ -0,0 +1,21 @@
+package chiramberro2;
+import java.util.*;
+public class asdf {
+	
+
+	 
+	    public static void main(String[] args) {
+	    	double longitud;
+	        double area;
+	        Scanner sc= new Scanner(System.in);
+	        
+	        System.out.println("inserte el radio");
+	        double radio = sc.nextInt();
+	        area = (3.141592 * radio * radio);
+	        longitud = (2 * 3.141592 * radio);
+	        System.out.println("el area de la circunferencia es "+area);
+	        System.out.println("la longitud de la circunferencia es "+longitud);
+	        
+	        
+}
+}
