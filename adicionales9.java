573b9860d49a4a2584f95b6bec01b3eafe07b80a
Magdalene <127776996+Magdalenewe@users.noreply.github.com>
2023-04-03 12:29:46 -0300

    Add files via upload

---
+++ adicionales9.java
@@ -0,0 +1,18 @@
+package chiramberro2;
+import java.util.Scanner;
+public class adicionales9 {
+
+	    public static void main(String[] args) {
+	        
+	    	 Scanner sc = new Scanner(System.in);
+	         int N;
+	         System.out.print("Introduzca valor de N: ");
+	         N = sc.nextInt(); //supondremos que el número introducido tiene 5 cifras                                  
+	         System.out.println(N/10000);
+	         System.out.println(N/1000);
+	         System.out.println(N/100);
+	         System.out.println(N/10);
+	         System.out.println(N);  
+	    }
+	}
+	
