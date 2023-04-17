087241c265cad6e197f66fc1ad4fa40f241b2d9f
Magdalene <127776996+Magdalenewe@users.noreply.github.com>
2023-04-17 11:45:59 -0300

    Add files via upload

---
+++ dos6.java
@@ -0,0 +1,29 @@
+package chiramberro2;
+
+import java.util.Scanner;
+
+public class dos6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	  Scanner sc=new Scanner(System.in);
+   
+   	  System.out.println("ingrese un numero ");
+   	  int n1=sc.nextInt();
+   	  System.out.println("ingrese un numero ");
+ 	  int n2=sc.nextInt();
+   
+   	  
+	  if (n1 < n2 ){
+		  System.out.println(n2+""+n1);
+	  }
+	  else if (n1 > n2) {
+		  System.out.println(n1+""+n2);
+	  }
+	  else {
+		  System.out.println("moron");
+	  }
+	}
+
+}
